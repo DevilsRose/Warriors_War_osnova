e7247b0ba9a9d1364e09e3444c4e6750c5a3d306
DevilsRose <104551205+DevilsRose@users.noreply.github.com>
2022-10-15 18:23:49 +0300

    v.4

---
+++ Assets/Scripts/Enemy/Firetrap.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Firetrap : MonoBehaviour
+{
+    [SerializeField] private float damage;
+
+    [Header("Firetrap Timers")]
+    [SerializeField] private float activationDelay;
+    [SerializeField] private float activeTime;
+    private Animator anim;
+    private SpriteRenderer spriteRend;
+
+    private bool triggered;
+    private bool active;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            if(!triggered)
+            {
+
+            }
+
+            if(active)
+            {
+                collision.GetComponent<Health>().TakeDamage(damage);
+            }
+        }
+    }
+
+
+}
